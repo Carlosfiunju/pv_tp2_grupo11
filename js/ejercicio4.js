3991663c2e505cd3737077295ec2b052305ed5d4
santtiago8 <46324472@fi.unju.edu.ar>
2025-04-16 00:27:06 -0300

    agrego mis puntos

---
+++ js/ejercicio4.js
@@ -0,0 +1,5 @@
+let lado = 4 ;
+  let area = 2 * lado;
+  let perimetro = lado + lado + lado + lado 
+  console.log("el area del cuadrado es:", area)
+  console.log("el perimetro del cuadrado es: ", perimetro)
