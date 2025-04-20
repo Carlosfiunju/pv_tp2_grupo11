7660b0e395ea863bd9d53c60ab3eab876b15809a
Carlosfiunju <charlyaparicio03@gmail.com>
2025-04-20 12:22:45 -0300

    "Agrego el ejercicio n°3"

---
+++ js/ejercicio3.js
@@ -0,0 +1,6 @@
+//Definicion de variables
+const radio = 7.5;
+//Calculo
+let area = Math.PI*Math.pow(radio,2);
+//Muestra por consola
+console.lo("El area del circulo con radio "+radio+!" es: "+area);
