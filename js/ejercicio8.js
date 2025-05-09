05a7b69ffcc56e91ce0d60c078b0a66d2e2d34b5
Carlosfiunju <charlyaparicio03@gmail.com>
2025-05-09 20:48:13 -0300

    Agrego los puntos restantes

---
+++ js/ejercicio8.js
@@ -0,0 +1,14 @@
+const nummayor= (num1,num2)=> {
+ if (num1>num2) {
+    console.log("el numero" + num1 + "es mayor")
+  }
+   else if (num2>num1) {
+        console.log("el numero " + num2 + "es mayor" )
+     }
+      else{
+        console.log("los numeros son iguales")
+     }
+    }
+
+
+nummayor(3,3);
